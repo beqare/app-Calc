dbc6bbab0cc16c22c19da455901b743b4e82c88c
beqare <turtelmaster11@gmail.com>
2024-10-13 05:47:56 +0200

    release

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -54,170 +54,270 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 12);
+            textBox1.BackColor = Color.Black;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("SimSun", 20F);
+            textBox1.ForeColor = Color.FromArgb(153, 153, 153);
+            textBox1.Location = new Point(13, 12);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(360, 23);
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(360, 38);
             textBox1.TabIndex = 0;
             // 
             // num1
             // 
-            num1.Location = new Point(12, 193);
+            num1.AutoSize = true;
+            num1.BackColor = Color.FromArgb(34, 34, 34);
+            num1.Cursor = Cursors.Hand;
+            num1.FlatStyle = FlatStyle.Flat;
+            num1.Font = new Font("SimSun", 30F);
+            num1.ForeColor = Color.FromArgb(153, 153, 153);
+            num1.Location = new Point(13, 208);
             num1.Name = "num1";
             num1.Size = new Size(113, 70);
             num1.TabIndex = 8;
             num1.Text = "1";
-            num1.UseVisualStyleBackColor = true;
+            num1.UseVisualStyleBackColor = false;
             num1.Click += numButton_Click;
             // 
             // num3
             // 
-            num3.Location = new Point(259, 193);
+            num3.AutoSize = true;
+            num3.BackColor = Color.FromArgb(34, 34, 34);
+            num3.Cursor = Cursors.Hand;
+            num3.FlatStyle = FlatStyle.Flat;
+            num3.Font = new Font("SimSun", 30F);
+            num3.ForeColor = Color.FromArgb(153, 153, 153);
+            num3.Location = new Point(260, 208);
             num3.Name = "num3";
             num3.Size = new Size(113, 70);
             num3.TabIndex = 9;
             num3.Text = "3";
-            num3.UseVisualStyleBackColor = true;
+            num3.UseVisualStyleBackColor = false;
             num3.Click += numButton_Click;
             // 
             // num2
             // 
-            num2.Location = new Point(136, 193);
+            num2.AutoSize = true;
+            num2.BackColor = Color.FromArgb(34, 34, 34);
+            num2.Cursor = Cursors.Hand;
+            num2.FlatStyle = FlatStyle.Flat;
+            num2.Font = new Font("SimSun", 30F);
+            num2.ForeColor = Color.FromArgb(153, 153, 153);
+            num2.Location = new Point(137, 208);
             num2.Name = "num2";
             num2.Size = new Size(113, 70);
             num2.TabIndex = 10;
             num2.Text = "2";
-            num2.UseVisualStyleBackColor = true;
+            num2.UseVisualStyleBackColor = false;
             num2.Click += numButton_Click;
             // 
             // num5
             // 
-            num5.Location = new Point(136, 117);
+            num5.AutoSize = true;
+            num5.BackColor = Color.FromArgb(34, 34, 34);
+            num5.Cursor = Cursors.Hand;
+            num5.FlatStyle = FlatStyle.Flat;
+            num5.Font = new Font("SimSun", 30F);
+            num5.ForeColor = Color.FromArgb(153, 153, 153);
+            num5.Location = new Point(137, 132);
             num5.Name = "num5";
             num5.Size = new Size(113, 70);
             num5.TabIndex = 13;
             num5.Text = "5";
-            num5.UseVisualStyleBackColor = true;
+            num5.UseVisualStyleBackColor = false;
             num5.Click += numButton_Click;
             // 
             // num6
             // 
-            num6.Location = new Point(259, 117);
+            num6.AutoSize = true;
+            num6.BackColor = Color.FromArgb(34, 34, 34);
+            num6.Cursor = Cursors.Hand;
+            num6.FlatStyle = FlatStyle.Flat;
+            num6.Font = new Font("SimSun", 30F);
+            num6.ForeColor = Color.FromArgb(153, 153, 153);
+            num6.Location = new Point(260, 132);
             num6.Name = "num6";
             num6.Size = new Size(113, 70);
             num6.TabIndex = 12;
             num6.Text = "6";
-            num6.UseVisualStyleBackColor = true;
+            num6.UseVisualStyleBackColor = false;
             num6.Click += numButton_Click;
             // 
             // num4
             // 
-            num4.Location = new Point(12, 117);
+            num4.AutoSize = true;
+            num4.BackColor = Color.FromArgb(34, 34, 34);
+            num4.Cursor = Cursors.Hand;
+            num4.FlatStyle = FlatStyle.Flat;
+            num4.Font = new Font("SimSun", 30F);
+            num4.ForeColor = Color.FromArgb(153, 153, 153);
+            num4.Location = new Point(13, 132);
             num4.Name = "num4";
             num4.Size = new Size(113, 70);
             num4.TabIndex = 11;
             num4.Text = "4";
-            num4.UseVisualStyleBackColor = true;
+            num4.UseVisualStyleBackColor = false;
             num4.Click += numButton_Click;
             // 
             // num8
             // 
-            num8.Location = new Point(136, 41);
+            num8.AutoSize = true;
+            num8.BackColor = Color.FromArgb(34, 34, 34);
+            num8.Cursor = Cursors.Hand;
+            num8.FlatStyle = FlatStyle.Flat;
+            num8.Font = new Font("SimSun", 30F);
+            num8.ForeColor = Color.FromArgb(153, 153, 153);
+            num8.Location = new Point(137, 56);
             num8.Name = "num8";
             num8.Size = new Size(113, 70);
             num8.TabIndex = 16;
             num8.Text = "8";
-            num8.UseVisualStyleBackColor = true;
+            num8.UseVisualStyleBackColor = false;
             num8.Click += numButton_Click;
             // 
             // num9
             // 
-            num9.Location = new Point(259, 41);
+            num9.AutoSize = true;
+            num9.BackColor = Color.FromArgb(34, 34, 34);
+            num9.Cursor = Cursors.Hand;
+            num9.FlatStyle = FlatStyle.Flat;
+            num9.Font = new Font("SimSun", 30F);
+            num9.ForeColor = Color.FromArgb(153, 153, 153);
+            num9.Location = new Point(260, 56);
             num9.Name = "num9";
             num9.Size = new Size(113, 70);
             num9.TabIndex = 15;
             num9.Text = "9";
-            num9.UseVisualStyleBackColor = true;
+            num9.UseVisualStyleBackColor = false;
             num9.Click += numButton_Click;
             // 
             // num7
             // 
-            num7.Location = new Point(12, 41);
+            num7.AutoSize = true;
+            num7.BackColor = Color.FromArgb(34, 34, 34);
+            num7.Cursor = Cursors.Hand;
+            num7.FlatStyle = FlatStyle.Flat;
+            num7.Font = new Font("SimSun", 30F);
+            num7.ForeColor = Color.FromArgb(153, 153, 153);
+            num7.Location = new Point(13, 56);
             num7.Name = "num7";
             num7.Size = new Size(113, 70);
             num7.TabIndex = 14;
             num7.Text = "7";
-            num7.UseVisualStyleBackColor = true;
+            num7.UseVisualStyleBackColor = false;
             num7.Click += numButton_Click;
             // 
             // minus
             // 
-            minus.Location = new Point(259, 269);
+            minus.AutoSize = true;
+            minus.BackColor = Color.FromArgb(34, 34, 34);
+            minus.Cursor = Cursors.Hand;
+            minus.FlatStyle = FlatStyle.Flat;
+            minus.Font = new Font("SimSun", 30F);
+            minus.ForeColor = Color.FromArgb(255, 192, 192);
+            minus.Location = new Point(260, 284);
             minus.Name = "minus";
             minus.Size = new Size(56, 70);
             minus.TabIndex = 19;
             minus.Text = "-";
-            minus.UseVisualStyleBackColor = true;
+            minus.UseVisualStyleBackColor = false;
             minus.Click += operationButton_Click;
             // 
             // multiply
             // 
-            multiply.Location = new Point(321, 269);
+            multiply.AutoSize = true;
+            multiply.BackColor = Color.FromArgb(34, 34, 34);
+            multiply.Cursor = Cursors.Hand;
+            multiply.FlatStyle = FlatStyle.Flat;
+            multiply.Font = new Font("SimSun", 30F);
+            multiply.ForeColor = Color.FromArgb(255, 192, 192);
+            multiply.Location = new Point(322, 284);
             multiply.Name = "multiply";
             multiply.Size = new Size(51, 70);
             multiply.TabIndex = 18;
             multiply.Text = "*";
-            multiply.UseVisualStyleBackColor = true;
+            multiply.UseVisualStyleBackColor = false;
             multiply.Click += operationButton_Click;
             // 
             // plus
             // 
-            plus.Location = new Point(198, 269);
+            plus.AutoSize = true;
+            plus.BackColor = Color.FromArgb(34, 34, 34);
+            plus.Cursor = Cursors.Hand;
+            plus.FlatStyle = FlatStyle.Flat;
+            plus.Font = new Font("SimSun", 30F);
+            plus.ForeColor = Color.FromArgb(255, 192, 192);
+            plus.Location = new Point(199, 284);
             plus.Name = "plus";
             plus.Size = new Size(51, 70);
             plus.TabIndex = 17;
             plus.Text = "+";
-            plus.UseVisualStyleBackColor = true;
+            plus.UseVisualStyleBackColor = false;
             plus.Click += operationButton_Click;
             // 
             // clear
             // 
-            clear.Location = new Point(12, 345);
+            clear.AutoSize = true;
+            clear.BackColor = Color.FromArgb(34, 34, 34);
+            clear.Cursor = Cursors.Hand;
+            clear.FlatStyle = FlatStyle.Flat;
+            clear.Font = new Font("SimSun", 30F);
+            clear.ForeColor = Color.FromArgb(255, 224, 192);
+            clear.Location = new Point(13, 360);
             clear.Name = "clear";
             clear.Size = new Size(180, 70);
             clear.TabIndex = 22;
             clear.Text = "C";
-            clear.UseVisualStyleBackColor = true;
+            clear.UseVisualStyleBackColor = false;
             clear.Click += clear_Click;
             // 
             // result
             // 
-            result.Location = new Point(198, 345);
+            result.AutoSize = true;
+            result.BackColor = Color.FromArgb(34, 34, 34);
+            result.Cursor = Cursors.Hand;
+            result.FlatStyle = FlatStyle.Flat;
+            result.Font = new Font("SimSun", 30F);
+            result.ForeColor = Color.FromArgb(192, 255, 192);
+            result.Location = new Point(199, 360);
             result.Name = "result";
             result.Size = new Size(174, 70);
             result.TabIndex = 21;
             result.Text = "=";
-            result.UseVisualStyleBackColor = true;
+            result.UseVisualStyleBackColor = false;
             result.Click += result_Click;
             // 
             // divide
             // 
-            divide.ForeColor = SystemColors.ActiveCaptionText;
-            divide.Location = new Point(136, 269);
+            divide.AutoSize = true;
+            divide.BackColor = Color.FromArgb(34, 34, 34);
+            divide.Cursor = Cursors.Hand;
+            divide.FlatStyle = FlatStyle.Flat;
+            divide.Font = new Font("SimSun", 30F);
+            divide.ForeColor = Color.FromArgb(255, 192, 192);
+            divide.Location = new Point(137, 284);
             divide.Name = "divide";
             divide.Size = new Size(56, 70);
             divide.TabIndex = 20;
             divide.Text = "/";
-            divide.UseVisualStyleBackColor = true;
+            divide.UseVisualStyleBackColor = false;
             divide.Click += operationButton_Click;
             // 
             // num0
             // 
-            num0.Location = new Point(12, 269);
+            num0.AutoSize = true;
+            num0.BackColor = Color.FromArgb(34, 34, 34);
+            num0.Cursor = Cursors.Hand;
+            num0.FlatStyle = FlatStyle.Flat;
+            num0.Font = new Font("SimSun", 30F);
+            num0.ForeColor = Color.FromArgb(153, 153, 153);
+            num0.Location = new Point(13, 284);
             num0.Name = "num0";
             num0.Size = new Size(113, 70);
             num0.TabIndex = 23;
             num0.Text = "0";
-            num0.UseVisualStyleBackColor = true;
+            num0.UseVisualStyleBackColor = false;
             num0.Click += numButton_Click;
             // 
             // Form1
@@ -226,7 +326,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             BackColor = Color.FromArgb(17, 17, 17);
-            ClientSize = new Size(384, 429);
+            ClientSize = new Size(384, 445);
             Controls.Add(num0);
             Controls.Add(clear);
             Controls.Add(result);
@@ -244,6 +344,8 @@
             Controls.Add(num3);
             Controls.Add(num1);
             Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Rechner";
